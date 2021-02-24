e8332e8effa3e4ef799770ddcf72615486b8dfa9
Chewbacca <brokenempirebe@gmail.com>
2021-02-24 18:45:09 +0100

    Mobile Recipes

--- commands/fun/recipe.js
+++ commands/fun/recipe.js
@@ -1,5 +1,5 @@
 const recipe = require('../../recipe.json');
-const possibleModes = ['normal', 'cac', 'pizza'];
+const possibleModes = ['normal', 'cac', 'pizza', 'mobile'];
 
 function getRandom(min, max) {
 	return Math.floor(Math.random() * (max - min) + min);
@@ -8,12 +8,15 @@ function getRandom(min, max) {
 module.exports = {
 	name: 'recipe',
 	description: 'Sends a random recipe you should use from the specified gamemode!',
-	usage: '`recipe normal|cac|pizza`',
+	usage: '`recipe mobile|normal|cac|pizza`',
 	async execute(client, message, args) {
-		if (args.length <= 0) return message.reply('You need to specify a mode!\n`recipe normal|cac|pizza`');
-		if (!possibleModes.includes(args[0].toLowerCase())) return message.reply('You need to specify a mode!\n`recipe normal|cac|pizza`');
+		if (args.length <= 0) return message.reply('You need to specify a mode!\n`recipe mobile|normal|cac|pizza`');
+		if (!possibleModes.includes(args[0].toLowerCase())) return message.reply('You need to specify a mode!\n`recipe mobile|normal|cac|pizza`');
 		let mode;
 		switch (args[0].toLowerCase()) {
+		case 'mobile':
+			mode = recipe.mobile;
+			break;
 		case 'normal':
 			mode = recipe.normal;
 			break;
