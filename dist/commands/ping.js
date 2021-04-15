62b37a8d873c2b470d779c6f4d843efc57e587fa
Chewbacca <mail@thehairy.org>
2021-04-15 18:55:16 +0200

    ignore dist

--- dist/commands/ping.js
+++
@@ -1,9 +0,0 @@
-export const name = 'ping';
-export const create = {
-    name: 'ping',
-    description: 'Ping Pong!',
-};
-export const execute = (client, interaction) => {
-    interaction.editReply(':tada: | Still alive!');
-};
-//# sourceMappingURL=ping.js.map
